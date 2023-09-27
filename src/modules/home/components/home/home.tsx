0561be048bbba2f412b53a81dfdffb50293ba509
Joana Valente <108897611+joanapaisvalente@users.noreply.github.com>
2023-09-27 22:28:16 +0100

    loading spinner

--- src/modules/home/components/home/home.tsx
+++ src/modules/home/components/home/home.tsx
@@ -8,7 +8,10 @@ export const Home: React.FC = () => {
   return (
     <>
       {isUsersLoading ? (
-        <div>{t("home.main.loading")}</div>
+        <div className="flex items-center flex-col">
+          <div className="h-14 w-14 border-solid border-b-4 border-slate-600 rounded-full animate-spin my-4"></div>
+          <div>{t("home.main.loading")}</div>
+        </div>
       ) : (
         <UsersList usersList={users} />
       )}
